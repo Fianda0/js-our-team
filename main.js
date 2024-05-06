0f55cd87f63bfd552ed863c45a555425fe4691ac
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-05-06 15:14:01 +0200

    Bonus 1

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 let persone = [
-    { nome: 'Wayne Barnett', ruolo: 'Founder & CEO', immagine: 'angela-caroll-chief-editor.jpg' },
-    { nome: 'Angela Caroll', ruolo: 'Chief Editor', immagine: 'angela-caroll-chief-editor.jpg' },
-    { nome: 'Walter Gordon', ruolo: 'Office Manager', immagine: 'walter-gordon-office-manager.jpg' },
-    { nome: 'Angela Lopez', ruolo: 'Social Media Manager', immagine: 'angela-lopez-social-media-manager.jpg' },
-    { nome: 'Scott Estrada', ruolo: 'Developer', immagine: 'scott-estrada-developer.jpg' },
-    { nome: 'Babarra Ramos', ruolo: 'Graphic Designer', immagine: 'barbara-ramos-graphic-designer.jpg' },
+    {
+        nome: 'Wayne Barnett', ruolo: 'Founder & CEO', immagine: '<img src="./img/wayne-barnett-founder-ceo.jpg">'
+    },
+    { nome: 'Angela Caroll', ruolo: 'Chief Editor', immagine: '<img src="./img/angela-caroll-chief-editor.jpg">' },
+    { nome: 'Walter Gordon', ruolo: 'Office Manager', immagine: '<img src="./img/walter-gordon-office-manager.jpg">' },
+    { nome: 'Angela Lopez', ruolo: 'Social Media Manager', immagine: '<img src="./img/angela-lopez-social-media-manager.jpg">' },
+    { nome: 'Scott Estrada', ruolo: 'Developer', immagine: '<img src="./img/scott-estrada-developer.jpg">' },
+    { nome: 'Babarra Ramos', ruolo: 'Graphic Designer', immagine: '<img src="./img/barbara-ramos-graphic-designer.jpg">' },
 ];
 
 for (let i = 0; i < persone.length; i++) {
     const element = persone[i];
     for (let key in persone[i]) {
         console.log(key, persone[i][key])
-        document.getElementById('membri').innerHTML += `<div> ${key} ${persone[i][key]}</div>`
+        document.getElementById('membri').innerHTML += `<div>${persone[i][key]}</div>`
     }
 }
